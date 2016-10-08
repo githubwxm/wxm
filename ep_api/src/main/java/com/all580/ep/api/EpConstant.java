74cf9af0a2ccb5daaa4b675f0e7cc99bd56eaa0c
wangximing <wangxm@all580.com>
2016-10-08 14:48:49 +0800

    添加平台商企业

--- ep_api/src/main/java/com/all580/ep/api/EpConstant.java
+++ ep_api/src/main/java/com/all580/ep/api/EpConstant.java
@@ -5,9 +5,9 @@ package com.all580.ep.api;
  */
 public class EpConstant {
     public static class EpStatus{
-        public static Integer uninitial=0;
-        public static Integer active=1;
-        public static Integer freeze=2;
-        public static Integer stop=3;
+        public static Integer uninitial=0;//未初始化
+        public static Integer active=1;//激活
+        public static Integer freeze=2;//冻结
+        public static Integer stop=3;//停用
     }
 }
