c8b6768d9093634d9d096fe0a7cb6181166822b8
wangximing <wangxm@all580.com>
2016-10-08 18:50:20 +0800

    添加平台商企业

--- ep_api/src/main/java/com/all580/ep/api/EpConstant.java
+++ ep_api/src/main/java/com/all580/ep/api/EpConstant.java
@@ -5,17 +5,17 @@ package com.all580.ep.api;
  */
 public class EpConstant {
     public static class EpStatus{
-        public static Integer uninitial=0;//未初始化
-        public static Integer active=1;//激活
-        public static Integer freeze=2;//冻结
-        public static Integer stop=3;//停用
+        public static final Integer  UNINITIAL =0;//未初始化
+        public static final Integer  ACTIVE =1;//激活
+        public static final Integer  FREEZE=2;//冻结
+        public static final Integer STOP=3;//停用
     }
     public static class PaymentType{
-        public static Integer balance=1;//余额
-        public static Integer aliPay=2;//支付宝
-        public static Integer weixin=3;//微信
-        public static Integer bank=4;//银行
-        public static Integer normal=1;// 正常
-        public static Integer stop=1;// 停用
+        public static final Integer BALANCE=1;//余额
+        public static final Integer ALI_PAY=2;//支付宝
+        public static final Integer WE_CHAT=3;//微信
+        public static final Integer BANK=4;//银行
+        public static final Integer STATUS_NORMAL =1;// 正常
+        public static final Integer STATUS_STOP=2;// 停用
     }
 }
