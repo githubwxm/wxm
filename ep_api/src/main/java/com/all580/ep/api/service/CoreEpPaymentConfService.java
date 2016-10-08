eb3c375041664fc3c22e23df709f62f37aaf18a5
wangximing <wangxm@all580.com>
2016-10-08 21:02:11 +0800

    添加平台商企业

--- ep_api/src/main/java/com/all580/ep/api/service/CoreEpPaymentConfService.java
+++ ep_api/src/main/java/com/all580/ep/api/service/CoreEpPaymentConfService.java
@@ -10,12 +10,32 @@ import java.util.Map;
 public interface CoreEpPaymentConfService {
 
    /**
-    *
-    * @param 初始余额配置
+    *初始支付方式配置
+    * @param
     * @return
      */
    Result<Integer> create(Map params);
+
+    /**
+     * 添加收款方式配置
+     * @param map
+     * @return
+     */
    Result<Integer> add(Map map);
+
+   /**
+    *修改收款方式配置
+    * @param map
+    * @return
+     */
+   Result<Integer> update(Map map);
+
+   /**
+    * 更新状态
+    * @param map
+    * @return
+     */
+   Result<Integer> updateStatus(Map map);
    CoreEpPaymentConf findById( Integer core_ep_id);
    List<CoreEpPaymentConf> isExists(Map map);
 }
