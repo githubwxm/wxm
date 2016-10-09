aa72afca0b5ab89c3d80924b3dbef1ced4d2eee1
wangximing <wangxm@all580.com>
2016-10-09 21:35:14 +0800

    添加平台商企业

--- ep_api/src/main/java/com/all580/ep/api/service/CoreEpAccessService.java
+++ ep_api/src/main/java/com/all580/ep/api/service/CoreEpAccessService.java
@@ -9,6 +9,6 @@ import java.util.Map;
  * Created by Administrator on 2016/9/29 0029.
  */
 public interface CoreEpAccessService {
-    Result<?> create(Map map);
+    Result<Integer> create(Map map);
     Result<CoreEpAccess> select(Map params);
 }
