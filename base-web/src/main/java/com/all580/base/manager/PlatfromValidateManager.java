ba3f421f9f0196fecb1f5475372a3223253442eb
wangximing <wangxm@all580.com>
2016-10-11 19:05:15 +0800

    添加平台商企业

---
+++ base-web/src/main/java/com/all580/base/manager/PlatfromValidateManager.java
@@ -0,0 +1,62 @@
+package com.all580.base.manager;
+
+import com.framework.common.validate.ValidRule;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Administrator on 2016/10/11 0011.
+ *
+ */
+public class PlatfromValidateManager {
+
+
+    public Map<String[], ValidRule[]> generateCreateEpValidate() {
+        Map<String[], ValidRule[]> rules = new HashMap<>();
+        // 校验不为空的参数
+        rules.put(new String[]{
+                "name.", // 企业中文名
+                "code", // '企业组织机构代码',
+                "logo_pic", // '企业组织机构代码',
+                "license", // 营业证编号
+                "linkman", // 企业联系人姓名
+                "link_phone", // 企业联系人电话
+                "province", // 企业省
+                "city", // 市
+                "area", // 区
+                "address", // 详细地址
+        }, new ValidRule[]{new ValidRule.NotNull()});
+//
+//        // 校验整数
+        rules.put(new String[]{
+                "province", // 企业省
+                "city", // 市
+                "area", // 区
+        }, new ValidRule[]{new ValidRule.Digits()});
+
+//        // 校验身份证
+//        rules.put(new String[]{
+//                "items.visitor.sid" // 订单游客身份证号码
+//        }, new ValidRule[]{new ValidRule.IdCard()});
+        // 校验手机号码
+        rules.put(new String[]{
+                "link_phone", // 订单联系人手机号码
+        }, new ValidRule[]{new ValidRule.Pattern(ValidRule.MOBILE_PHONE)});
+        return rules;
+    }
+    private Map<String[], ValidRule[]> generateCreateStatusValidate() {
+        Map<String[], ValidRule[]> rules = new HashMap<>();
+        // 校验不为空的参数
+        rules.put(new String[]{
+                "id.", //
+                "access_id"
+        }, new ValidRule[]{new ValidRule.NotNull()});
+
+        // 校验整数
+        rules.put(new String[]{
+                "id" // 企业id
+        }, new ValidRule[]{new ValidRule.Digits()});
+        return rules;
+    }
+}
