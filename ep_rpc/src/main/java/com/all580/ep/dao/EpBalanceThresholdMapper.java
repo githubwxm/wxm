76bfaaa52e13c25cc514fe2eece0ccc0644915e2
wangximing <wangxm@all580.com>
2016-10-12 19:07:23 +0800

    添加余额支付

---
+++ ep_rpc/src/main/java/com/all580/ep/dao/EpBalanceThresholdMapper.java
@@ -0,0 +1,12 @@
+package com.all580.ep.dao;
+
+
+import java.util.Map;
+
+/**
+ * Created by Administrator on 2016/9/28 0028.
+ */
+public interface EpBalanceThresholdMapper {
+   int createOrUpdate(Map map);
+    Map<String,Object> select(Map map);
+}
