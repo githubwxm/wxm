1916a7999e3bcda09616493b1a50641eea20c248
zxjun <715213471@qq.com>
2016-10-18 10:48:37 +0800

    添加任务调度器版本

---
+++ order-api/src/main/java/com/all580/order/api/service/BookingOrderService.java
@@ -0,0 +1,41 @@
+package com.all580.order.api.service;
+
+import com.framework.common.Result;
+
+import java.util.Map;
+
+/**
+ * @author zhouxianjun(Alone)
+ * @ClassName:
+ * @Description: 预定服务
+ * @date 2016/9/27 8:52
+ */
+public interface BookingOrderService {
+    /**
+     * 创建订单
+     * @param params
+     * @return
+     */
+    Result<?> create(Map params);
+
+    /**
+     * 供应商审核订单
+     * @param params
+     * @return
+     */
+    Result<?> audit(Map params);
+
+    /**
+     * 订单支付
+     * @param params
+     * @return
+     */
+    Result<?> payment(Map params);
+
+    /**
+     * 重新发码
+     * @param params
+     * @return
+     */
+    Result<?> resendTicket(Map params);
+}
