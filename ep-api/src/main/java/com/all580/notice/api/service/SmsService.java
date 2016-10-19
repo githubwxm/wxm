524860f786da7b307a9b0c1d2ea1be8ed262d727
panyi5202 <panyi5202@163.com>
2016-10-19 10:03:43 +0800

    修改短信接口

--- ep-api/src/main/java/com/all580/notice/api/service/SmsService.java
+++ ep-api/src/main/java/com/all580/notice/api/service/SmsService.java
@@ -9,14 +9,15 @@ import java.util.Map;
  * @since V0.0.1
  */
 public interface SmsService {
+
     /**
      * 发送短信
      *
      * @param destPhoneNum 目的手机号码
-     * @param smsTpl       短信模板
-     * @param params       模板参数
-     * @param outTplId     外部模板ID
+     * @param smsType      短信类型 @see SmsType
+     * @param epId         企业ID
+     * @param params       短信参数
      * @return 发送是否成功
      */
-    public boolean send(String destPhoneNum, String smsTpl, Map<String, String> params, String outTplId);
+    boolean send(String destPhoneNum, Integer smsType, Integer epId, Map<String, String> params);
 }
