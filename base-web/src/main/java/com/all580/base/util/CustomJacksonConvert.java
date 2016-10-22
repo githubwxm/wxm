5468e8ff4c9b18cd4fc966041d11ca382db0b979
zxjun <715213471@qq.com>
2016-10-22 16:32:49 +0800

    msg -- message

--- base-web/src/main/java/com/all580/base/util/CustomJacksonConvert.java
+++ base-web/src/main/java/com/all580/base/util/CustomJacksonConvert.java
@@ -31,7 +31,7 @@ public class CustomJacksonConvert extends MappingJackson2HttpMessageConverter {
 			Result r = (Result) object;
 			Map<String, Object> result = new HashMap<>();
 			result.put("code", r.getCode() == null ? (r.isSuccess() ? Result.SUCCESS : Result.FAIL) : r.getCode());
-			result.put("msg", r.getError());
+			result.put("message", r.getError());
 			result.put("data", r.get());
 			result.put("sign", "");
 			super.writeInternal(result, outputMessage);
