fea473ed61829c8d1ad720a130a61a76e75be6f1
wangximing <wangxm@all580.com>
2016-10-24 11:45:19 +0800

    添加校验名字余电话，格式化地区

--- ep-rpc/src/main/java/com/all580/ep/service/EpBalanceThresholdServiceImple.java
+++ ep-rpc/src/main/java/com/all580/ep/service/EpBalanceThresholdServiceImple.java
@@ -3,13 +3,18 @@ package com.all580.ep.service;
 import com.all580.ep.api.service.EpBalanceThresholdService;
 import com.all580.ep.com.Common;
 import com.all580.ep.dao.EpBalanceThresholdMapper;
+import com.all580.ep.dao.EpMapper;
+import com.all580.notice.api.service.SmsService;
 import com.framework.common.Result;
 import com.framework.common.exception.ApiException;
+import com.framework.common.util.CommonUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Service
@@ -20,6 +25,11 @@ public class EpBalanceThresholdServiceImple implements EpBalanceThresholdService
     @Autowired
     private EpBalanceThresholdMapper coreEpAccessMapper;
 
+    @Autowired
+    private EpMapper epMapper;
+
+    @Autowired
+    private SmsService smsService;
 
     @Override
     public  Result<Integer> createOrUpdate(Map map) {
@@ -59,12 +69,26 @@ public class EpBalanceThresholdServiceImple implements EpBalanceThresholdService
                 log.error("未查询到阀值数据"+map);
                 throw new ApiException("添加更新余额阀值出错"+map);
             }
-            return balance<threshold;
+            if(balance<threshold){
+                //Todo  发送余额短信
+                Map epMap = new HashMap();
+                Integer ep_id= CommonUtil.objectParseInteger(map.get("ep_id"));
+                epMap.put("id",ep_id);//再看发送短信所需要的参数
+                 List<Map> list= epMapper.select(epMap);//   获取企业信息
+                 if(null==list){
+                     if(!list.isEmpty()){
+                       String destPhoneNum=  list.get(0).get("link_phone").toString();
+                         return smsService.send(destPhoneNum,-1,ep_id,null);//发送短信
+                     }
+                 }
+                //TODO  发送之后操作
+
+            }
         } catch (Exception e) {
             log.error("查询数据库出错", e);
             throw new ApiException("查询数据库出错", e);
         }
-
+        return false;
     }
 
 }
