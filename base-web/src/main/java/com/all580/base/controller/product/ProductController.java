1be6f4085cc2d84c7ba0a89399addba77257dbb3
zq <zmjq08@gmail.com>
2016-10-25 20:00:32 +0800

    update product controller

--- base-web/src/main/java/com/all580/base/controller/product/ProductController.java
+++ base-web/src/main/java/com/all580/base/controller/product/ProductController.java
@@ -1,13 +1,20 @@
 package com.all580.base.controller.product;
 
 import com.all580.base.manager.ProductValidateManager;
+import com.all580.product.api.consts.ProductConstants;
+import com.all580.product.api.model.ProductAndSubsInfo;
+import com.all580.product.api.model.ProductSceneryInfo;
+import com.all580.product.api.service.ProductRPCService;
 import com.all580.product.api.service.ProductSalesPlanRPCService;
 import com.framework.common.BaseController;
 import com.framework.common.Result;
+import com.framework.common.lang.JsonUtils;
+import com.framework.common.vo.Paginator;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -23,26 +30,59 @@ public class ProductController extends BaseController {
     @Resource
     ProductSalesPlanRPCService productSalesPlanRPCService;
 
+    @Resource
+    ProductRPCService productRPCService;
+
+    /**
+     * 添加景区主产品
+     * @param params
+     * @return
+     */
     @RequestMapping(value = "add", method = RequestMethod.POST)
     @ResponseBody
     public Result<?> addSceneryProduct(@RequestBody Map params) {
-
+        // TODO:验证入参
+        switch (Integer.valueOf(params.get("type").toString())) {
+            case ProductConstants.ProductType.SCENERY:
+                ProductSceneryInfo productSceneryInfo = initProductScenery((Map) params.get("props"));
+                productRPCService.addSceneryProduct(params.get("name").toString(), Integer.valueOf(params.get("epId").toString()), productSceneryInfo);
+                break;
+            default: return new Result<>(false, "产品类型不匹配");
+        }
         return new Result<>(true);
     }
 
-    @RequestMapping(value = "sub/add")
+    private ProductSceneryInfo initProductScenery(Map props) {
+        ProductSceneryInfo productSceneryInfo = new ProductSceneryInfo();
+        productSceneryInfo.setProvince(Integer.valueOf(props.get("province").toString()));
+        productSceneryInfo.setCity(Integer.valueOf(props.get("city").toString()));
+        productSceneryInfo.setArea(Integer.valueOf(props.get("area").toString()));
+        productSceneryInfo.setPcastr(props.get("pcastr").toString());
+        productSceneryInfo.setAddress(props.get("address").toString());
+        productSceneryInfo.setBlurb(props.get("blurb").toString());
+        productSceneryInfo.setBusinessTime(props.get("businessTime").toString());
+        productSceneryInfo.setTel(props.get("tel").toString());
+        productSceneryInfo.setMap(props.get("map").toString());
+        productSceneryInfo.setImgs(JsonUtils.toJson(props.get("imgs")));
+        productSceneryInfo.setLevel(Integer.valueOf(props.get("level").toString()));
+        productSceneryInfo.setTransitLine(props.get("transitLines").toString());
+        productSceneryInfo.setType(JsonUtils.toJson(props.get("type")));
+        return productSceneryInfo;
+    }
+
+    @RequestMapping(value = "sub/add", method = RequestMethod.POST)
     @ResponseBody
     public Result<?> addScenerySubProduct(@RequestBody Map params) {
         return new Result<>(true);
     }
 
-    @RequestMapping(value = "update")
+    @RequestMapping(value = "update", method = RequestMethod.POST)
     @ResponseBody
     public Result<?> updateSceneryProduct(@RequestBody Map params) {
         return null;
     }
 
-    @RequestMapping(value = "sub/update")
+    @RequestMapping(value = "sub/update", method = RequestMethod.POST)
     @ResponseBody
     public Result<?> updateScenerySubProduct(@RequestBody Map params) {
         return null;
@@ -55,8 +95,11 @@ public class ProductController extends BaseController {
      */
     @RequestMapping(value = "self/list")
     @ResponseBody
-    public Result<?> searchSelfProviderProduct(@RequestParam Integer epId) {
-        return null;
+    public Result<Paginator<ProductAndSubsInfo>> searchSelfProviderProduct(@RequestParam Integer epId, @RequestParam String productName,
+                                                                           @RequestParam String productSubName, @RequestParam Integer productType,
+                                                                           @RequestParam Integer recordStart, @RequestParam Integer recordCount) {
+       // TODO: 验证入参
+       return productRPCService.searchSelfProviderProductList(epId, productName, productSubName, recordStart, recordCount);
     }
 
     /**
