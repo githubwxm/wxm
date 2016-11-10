95d0773b09326be5e8250fee1622a667d0a08cee
wangximing <wangxm@all580.com>
2016-11-10 10:16:55 +0800

    添加权限

---
+++ ep-api/src/main/java/com/all580/role/api/service/FuncIntfService.java
@@ -0,0 +1,7 @@
+package com.all580.role.api.service;
+
+/**
+ * Created by wxming on 2016/11/10 0010.
+ */
+public interface FuncIntfService {
+}
---
+++ ep-api/src/main/java/com/all580/role/api/service/IntfService.java
@@ -0,0 +1,12 @@
+package com.all580.role.api.service;
+
+import java.util.Map;
+
+/**
+ * Created by wxming on 2016/11/10 0010.
+ */
+public interface IntfService {
+    int insertInft(Map<String,Object> params);
+    int updateInft(Map<String,Object> params);
+
+}
