6ed38b984a1bf3f5a60794758867ac306a80ef9e
wangximing <wangxm@all580.com>
2016-11-11 11:41:13 +0800

    添加权限

---
+++ ep-rpc/src/main/java/com/all580/role/service/IntfServiceImple.java
@@ -0,0 +1,62 @@
+package com.all580.role.service;
+
+import com.all580.role.api.service.IntfService;
+import com.all580.role.dao.IntfMapper;
+import com.framework.common.Result;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.lang.exception.ApiException;
+import java.util.Map;
+
+/**
+ * Created by wxming on 2016/11/10 0010.
+ */
+@Service
+@Transactional(rollbackFor = {Exception.class, RuntimeException.class})
+@Slf4j
+public class IntfServiceImple implements IntfService {
+
+    @Autowired
+    private IntfMapper intfMapper;
+
+
+
+    @Override
+    public Result insertInft(Map<String, Object> params) {
+        try {
+            intfMapper.insertIntf(params);
+           // funcIntfMapper.insertFuncIntf(params);
+        } catch (Exception e) {
+            log.error("添加接口异常", e);
+            new ApiException("添加接口异常");
+        }
+        return new Result(true);
+    }
+    @Override
+    public Result selectFuncId(long id) {
+        try {
+            intfMapper.selectFuncId(id);
+            // funcIntfMapper.insertFuncIntf(params);
+        } catch (Exception e) {
+            log.error("查询接口异常", e);
+            new ApiException("查询接口异常");
+        }
+        return new Result(true);
+    }
+
+    @Override
+    public Result deleteInft(long id) {
+        try {
+           // funcIntfMapper.deleteFuncIntf(id);
+
+            intfMapper.deleteByPrimaryKey(id);
+        } catch (Exception e) {
+            log.error("删除接口异常", e);
+            new ApiException("删除接口异常");
+        }
+        return new Result(true);
+    }
+}
