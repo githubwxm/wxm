79e4ddfbdc0bdd27da69e1b207291b7926e72a19
wangximing <wangxm@all580.com>
2016-11-23 11:53:57 +0800

    添加余额校验 发送短信

---
+++ ep-api/src/main/java/com/all580/notice/api/service/MnsSubscribeService.java
@@ -0,0 +1,12 @@
+package com.all580.notice.api.service;
+
+import com.framework.common.Result;
+
+import java.util.Date;
+
+/**
+ * Created by wxming on 2016/11/23 0023.
+ */
+public interface MnsSubscribeService {
+    Result process(String mnsMsgId, String content, Date createDate);
+}
