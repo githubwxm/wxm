006aa0478da7e86bbd3e4cccbabc28df2949e2f3
zq <zmjq08@gmail.com>
2016-12-01 10:40:16 +0800

    统一修改参数风格为下划线

--- base-web/src/main/java/com/all580/base/controller/product/PlanController.java
+++ base-web/src/main/java/com/all580/base/controller/product/PlanController.java
@@ -53,10 +53,10 @@ public class PlanController extends BaseController {
 
     private AddProductPlanParams initAddProductPlanParams(Map params) {
         AddProductPlanParams addProductPlanParams = new AddProductPlanParams();
-        addProductPlanParams.setProductSubId(CommonUtil.objectParseInteger(params.get("productSubId")));
-        addProductPlanParams.setEndDate(DateFormatUtils.converToDateTime(CommonUtil.objectParseString(params.get("endDate"))));
+        addProductPlanParams.setProduct_sub_id(CommonUtil.objectParseInteger(params.get("product_sub_id")));
+        addProductPlanParams.setEnd_date(DateFormatUtils.converToDateTime(CommonUtil.objectParseString(params.get("end_date"))));
         addProductPlanParams.setEpId(CommonUtil.objectParseInteger(params.get("ep_id")));
-        addProductPlanParams.setStartDate(DateFormatUtils.converToDateTime(CommonUtil.objectParseString(params.get("startDate"))));
+        addProductPlanParams.setStart_date(DateFormatUtils.converToDateTime(CommonUtil.objectParseString(params.get("start_date"))));
         if (params.get("stock") != null && !params.get("stock").toString().equals("")) {
             addProductPlanParams.setStock(CommonUtil.objectParseInteger(params.get("stock")));
         }
@@ -73,7 +73,7 @@ public class PlanController extends BaseController {
     @RequestMapping(value = "update", method = RequestMethod.POST)
     @ResponseBody
     public Result<?> updateSalesPlan(@RequestBody Map params) {
-        return productSalesPlanService.updateProductPlan(CommonUtil.objectParseInteger(params.get("productPlanId")), CommonUtil.objectParseInteger(params.get("stock")));
+        return productSalesPlanService.updateProductPlan(CommonUtil.objectParseInteger(params.get("product_plan_id")), CommonUtil.objectParseInteger(params.get("stock")));
     }
 
     /**
@@ -105,14 +105,14 @@ public class PlanController extends BaseController {
     @ResponseBody
     public Result<Paginator<Map<String, ?>>> canSaleSubProductList(
             @RequestParam("ep_id") Integer epId,
-            @RequestParam("productName") String productName,
-            @RequestParam("productSubName") String productSubName,
+            @RequestParam("product_name") String productName,
+            @RequestParam("product_sub_name") String productSubName,
             @RequestParam("province") Integer province,
             @RequestParam("city") Integer city,
             @RequestParam("area") Integer area,
-            @RequestParam("ticketFlag") Integer ticketFlag,
-            @RequestParam("payType") Integer payType,
-            @RequestParam("ticketDict") Integer ticketDict,
+            @RequestParam("ticket_flag") Integer ticketFlag,
+            @RequestParam("pay_type") Integer payType,
+            @RequestParam("ticket_dict") Integer ticketDict,
             @RequestParam("record_start") Integer start,
             @RequestParam("record_count") Integer count) {
         return productSalesPlanService.selectCanSaleSubProduct(epId,productName,productSubName,province,city,area,ticketFlag,payType,ticketDict,start,count);
@@ -126,9 +126,6 @@ public class PlanController extends BaseController {
     @RequestMapping(value = "sale/group", method = RequestMethod.POST)
     @ResponseBody
     public Result productSubDistributionGroup(@RequestBody Map params) {
-//        for (OnSalesParams param : initGroupOnsalesParams(params)) {
-//            productService.productOnSale(param);
-//        }
         return productService.productOnSaleToGroup(initGroupOnsalesParams(params));
     }
 
@@ -162,7 +159,7 @@ public class PlanController extends BaseController {
 
     private List<Map<String, Object>> initPlatformPlanSalesParams(Map params) {
         Map<String, Object> ep = epService.selectId(CommonUtil.objectParseInteger(params.get("platform_ep_id"))).get();
-        List<Map<String, Object>> onSales = (List<Map<String, Object>>) params.get("saledArray");
+        List<Map<String, Object>> onSales = (List<Map<String, Object>>) params.get("saled_array");
         for (Map<String, Object> planSale : onSales) {
             planSale.put("sale_ep_id", params.get("ep_id"));
             planSale.put("ep_id", ep.get("id"));
@@ -178,7 +175,7 @@ public class PlanController extends BaseController {
     }
 
     private List<Map<String, Object>> initPlatformPlanOffSalesParams(Map params) {
-        List<String> batches = (List<String>) params.get("offSaleArray");
+        List<String> batches = (List<String>) params.get("off_sale_array");
         List<Map<String, Object>> offSales = new ArrayList<>();
         for (String batch_id : batches) {
             Map<String, Object> ep = new HashMap<>();
@@ -213,35 +210,35 @@ public class PlanController extends BaseController {
         String name = CommonUtil.objectParseString(ep.get().get("name"));
         Map map = new HashMap();
         map.put("name",name);
-        map.put("saleEpId",params.get("ep_id"));
+        map.put("sale_ep_id",params.get("ep_id"));
         map.put("ep_id",ep_id);
-        map.put("priceType",params.get("priceType"));
-        map.put("pricePercent",params.get("pricePercent"));
-        map.put("pricePixed",params.get("pricePixed"));
-        map.put("productSubId",params.get("productSubId"));
+        map.put("price_type",params.get("price_type"));
+        map.put("price_percent",params.get("price_percent"));
+        map.put("price_pixed",params.get("price_pixed"));
+        map.put("product_sub_id",params.get("product_sub_id"));
         list.add(map);
         return list;
     }
     private List<Map> initEpOnsalesParams (Map params) {
-        List<Map> onSalesEps = (List<Map>) params.get("saledArray");
+        List<Map> onSalesEps = (List<Map>) params.get("saled_array");
         for (Map ep : onSalesEps) {
-            ep.put("productSubId", params.get("productSubId"));
-            ep.put("saleEpId", params.get("ep_id"));
+            ep.put("product_sub_id", params.get("product_sub_id"));
+            ep.put("sale_ep_id", params.get("ep_id"));
         }
         return onSalesEps;
     }
 
     private List<OnSalesParams> initGroupOnsalesParams (Map params) {
         List<OnSalesParams> onSalesParamses = new ArrayList<>();
-        List<Map> saledArray = (List<Map>) params.get("saledArray");
+        List<Map> saledArray = (List<Map>) params.get("saled_array");
         for (Map group : saledArray) {
             OnSalesParams onSalesParams = new OnSalesParams();
-            onSalesParams.setProductSubId(CommonUtil.objectParseInteger(params.get("productSubId")));
-            onSalesParams.setPricePixed(CommonUtil.objectParseInteger(group.get("pricePixed")));
-            onSalesParams.setPricePercent(CommonUtil.objectParseInteger(group.get("pricePercent")));
-            onSalesParams.setPriceType(CommonUtil.objectParseInteger(group.get("priceType")));
-            onSalesParams.setSaleEpId(CommonUtil.objectParseInteger(params.get("ep_id")));
-            onSalesParams.setGroupId(CommonUtil.objectParseInteger(group.get("groupId")));
+            onSalesParams.setProduct_sub_id(CommonUtil.objectParseInteger(params.get("product_sub_id")));
+            onSalesParams.setPrice_pixed(CommonUtil.objectParseInteger(group.get("price_pixed")));
+            onSalesParams.setPrice_percent(CommonUtil.objectParseInteger(group.get("price_percent")));
+            onSalesParams.setPrice_type(CommonUtil.objectParseInteger(group.get("price_type")));
+            onSalesParams.setSale_ep_id(CommonUtil.objectParseInteger(params.get("ep_id")));
+            onSalesParams.setGroup_id(CommonUtil.objectParseInteger(group.get("group_id")));
             onSalesParamses.add(onSalesParams);
         }
         return onSalesParamses;
@@ -266,7 +263,7 @@ public class PlanController extends BaseController {
     @RequestMapping(value = "off_sale/ep", method = RequestMethod.POST)
     @ResponseBody
     public Result productSubOffSaleEp(@RequestBody Map params) {
-        List<Map> paramList = (List<Map>) params.get("offSaleArray");
+        List<Map> paramList = (List<Map>) params.get("off_sale_array");
         if (paramList == null || paramList.isEmpty()) return new Result(false, "参数错误");
         return productService.productOffSaleBatch(params);
     }
@@ -279,7 +276,7 @@ public class PlanController extends BaseController {
     @RequestMapping(value = "off_sale/group", method = RequestMethod.POST)
     @ResponseBody
     public Result productSubOffSaleGroup(@RequestBody Map params) {
-        List<Map> paramList = (List<Map>) params.get("offSaleArray");
+        List<Map> paramList = (List<Map>) params.get("off_sale_array");
         if (paramList == null || paramList.isEmpty()) return new Result(false, "参数错误");
         return productService.productOffSaleGroup(params);
     }
