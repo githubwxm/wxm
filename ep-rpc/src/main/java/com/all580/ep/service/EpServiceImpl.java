9f9ffddb368c7deb9014523d5c02ebfa7319e990
wangximing <wangxm@all580.com>
2016-12-01 10:36:30 +0800

    平台商停用冻结发送短信

--- ep-rpc/src/main/java/com/all580/ep/service/EpServiceImpl.java
+++ ep-rpc/src/main/java/com/all580/ep/service/EpServiceImpl.java
@@ -491,17 +491,17 @@ public class EpServiceImpl implements EpService {
         params.put("status", EpConstant.EpStatus.FREEZE);
         try {
             Result<Integer> result = updatePlatfrom(params);
-//            if (result.isSuccess()) {
-//                String destPhoneNum = selectPhone(CommonUtil.objectParseInteger(map.get("id"))).get();
-//                int ep_id = CommonUtil.objectParseInteger(map.get("ep_id"));
-//                Map<String, String> smsParams = new HashMap<>();
-//                smsParams.put("dianhuahaoma", SmsType.Ep.CHANGLV_SERVICE_PHONE);//客户
-//                Result r = smsService.send(destPhoneNum, SmsType.Ep.PLATFORM_FREEZE, ep_id, smsParams);//发送短信
-//                if (!r.isSuccess()) {
-//                    log.warn("冻结平台商发送消息失败");
-//                    throw new ApiException("冻结平台商发送消息失败");
-//                }
-//            }
+            if (result.isSuccess()) {
+                String destPhoneNum = selectPhone(CommonUtil.objectParseInteger(map.get("id"))).get();
+                int ep_id = CommonUtil.objectParseInteger(map.get("ep_id"));
+                Map<String, String> smsParams = new HashMap<>();
+                smsParams.put("dianhuahaoma", SmsType.Ep.CHANGLV_SERVICE_PHONE);//客户
+                Result r = smsService.send(destPhoneNum, SmsType.Ep.PLATFORM_FREEZE, ep_id, smsParams);//发送短信
+                if (!r.isSuccess()) {
+                    log.warn("冻结平台商发送消息失败");
+                    throw new ApiException("冻结平台商发送消息失败");
+                }
+            }
             return result;
         } catch (ApiException e) {
             log.error(e.getMessage(), e);
@@ -518,17 +518,17 @@ public class EpServiceImpl implements EpService {
         params.put("status", EpConstant.EpStatus.STOP);
         try {
             Result<Integer> result = updatePlatfrom(params);
-//            if (result.isSuccess()) {
-//                String destPhoneNum = selectPhone(CommonUtil.objectParseInteger(map.get("id"))).get();
-//                int ep_id = CommonUtil.objectParseInteger(map.get("ep_id"));
-//                Map<String, String> smsParams = new HashMap<>();
-//                smsParams.put("dianhuahaoma", SmsType.Ep.CHANGLV_SERVICE_PHONE);//客户
-//                Result r = smsService.send(destPhoneNum, SmsType.Ep.PLATFORM_STOP, ep_id, smsParams);//发送短信
-//                if (!r.isSuccess()) {
-//                    log.warn("停用平台商发送消息失败");
-//                    throw new ApiException("停用平台商发送消息失败");
-//                }
-//            }
+            if (result.isSuccess()) {
+                String destPhoneNum = selectPhone(CommonUtil.objectParseInteger(map.get("id"))).get();
+                int ep_id = CommonUtil.objectParseInteger(map.get("ep_id"));
+                Map<String, String> smsParams = new HashMap<>();
+                smsParams.put("dianhuahaoma", SmsType.Ep.CHANGLV_SERVICE_PHONE);//客户
+                Result r = smsService.send(destPhoneNum, SmsType.Ep.PLATFORM_STOP, ep_id, smsParams);//发送短信
+                if (!r.isSuccess()) {
+                    log.warn("停用平台商发送消息失败");
+                    throw new ApiException("停用平台商发送消息失败");
+                }
+            }
             return result;
 
         } catch (ApiException e) {
