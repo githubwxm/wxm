742ccf727cf063effdeb5c35debec4163224d569
wangximing <wangxm@all580.com>
2016-12-09 15:59:01 +0800

    添加为空

--- ep-api/src/main/java/com/all580/ep/api/service/EpFinanceService.java
+++ ep-api/src/main/java/com/all580/ep/api/service/EpFinanceService.java
@@ -8,6 +8,19 @@ import java.util.Map;
  * Created by wxming on 2016/10/21 0021.
  */
 public interface EpFinanceService {
+
+
+    /**
+     * 获取企业信息及银行卡信息
+     * @param id
+     * @return
+     */
+    Result<Map<String,Object>> selectBank(int id);
+    /**
+     *  添加银行信息
+     *  ep_id bank_username  bank_name_address  bank_num
+     */
+    Result<Integer> addBank(Map<String,Object> map);
     /**
      * 查询企业信息合并余额信息
      * @param map
