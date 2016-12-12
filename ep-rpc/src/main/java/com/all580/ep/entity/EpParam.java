9a23ed81ab9490a7068223d13878a852cbe5b888
wangximing <wangxm@all580.com>
2016-12-12 14:03:01 +0800

    添加为空

---
+++ ep-rpc/src/main/java/com/all580/ep/entity/EpParam.java
@@ -0,0 +1,128 @@
+package com.all580.ep.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class EpParam implements Serializable {
+    private Integer id;
+
+    private Integer pid;
+
+    private String category;
+
+    private String name;
+
+    private String refCode;
+
+    private Long seq;
+
+    private String param;
+
+    private Integer status;
+
+    private Date createTime;
+
+    private Long userId;
+
+    private Integer paramType;
+
+    private String alias;
+
+    private static final long serialVersionUID = 1L;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getPid() {
+        return pid;
+    }
+
+    public void setPid(Integer pid) {
+        this.pid = pid;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category == null ? null : category.trim();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public String getRefCode() {
+        return refCode;
+    }
+
+    public void setRefCode(String refCode) {
+        this.refCode = refCode == null ? null : refCode.trim();
+    }
+
+    public Long getSeq() {
+        return seq;
+    }
+
+    public void setSeq(Long seq) {
+        this.seq = seq;
+    }
+
+    public String getParam() {
+        return param;
+    }
+
+    public void setParam(String param) {
+        this.param = param == null ? null : param.trim();
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Integer getParamType() {
+        return paramType;
+    }
+
+    public void setParamType(Integer paramType) {
+        this.paramType = paramType;
+    }
+
+    public String getAlias() {
+        return alias;
+    }
+
+    public void setAlias(String alias) {
+        this.alias = alias == null ? null : alias.trim();
+    }
+}
