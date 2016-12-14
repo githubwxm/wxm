0597270bee13614ff5ba75e3324b9fd81cc535fb
wangximing <wangxm@all580.com>
2016-12-14 11:23:28 +0800

    添加导出

---
+++ ep-api/src/main/java/com/all580/notice/api/service/FundChangeSubscribeService.java
@@ -0,0 +1,9 @@
+package com.all580.notice.api.service;
+
+import com.framework.common.mns.MnsSubscribeAction;
+
+/**
+ * Created by wxming on 2016/11/23 0023.
+ */
+public interface FundChangeSubscribeService extends MnsSubscribeAction {
+}
