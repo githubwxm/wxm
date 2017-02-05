6db1fe941e8a46aaf91a522470bf2bf874f1ce29
wangximing <wangxm@all580.com>
2017-02-05 16:11:35 +0800

    修改事件

--- ep-api/src/main/java/com/all580/notice/api/service/BalanceChangeSubscribeService.java
+++ ep-api/src/main/java/com/all580/notice/api/service/BalanceChangeSubscribeService.java
@@ -1,9 +1,12 @@
 package com.all580.notice.api.service;
 
+import com.all580.ep.api.conf.EpConstant;
+import com.framework.common.event.EventService;
 import com.framework.common.mns.MnsSubscribeAction;
 
 /**
  * Created by wxming on 2016/11/23 0023.
  */
+@EventService(EpConstant.Event.BALANCE_CHANGE)
 public interface BalanceChangeSubscribeService extends MnsSubscribeAction {
 }
