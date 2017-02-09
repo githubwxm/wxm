675575d387f3517c8a65168135e690a6c43ad40c
zxjun <715213471@qq.com>
2017-02-09 10:13:44 +0800

    修改事件订阅器

--- order-rpc/src/main/java/com/all580/order/service/PaymentCallbackServiceImpl.java
+++ order-rpc/src/main/java/com/all580/order/service/PaymentCallbackServiceImpl.java
@@ -5,11 +5,14 @@ import com.all580.order.api.service.PaymentCallbackService;
 import com.all580.order.dao.OrderMapper;
 import com.all580.order.entity.Order;
 import com.all580.order.manager.BookingOrderManager;
+import com.all580.order.manager.LockTransactionManager;
 import com.framework.common.Result;
+import com.framework.common.distributed.lock.DistributedLockTemplate;
+import com.framework.common.distributed.lock.DistributedReentrantLock;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -25,12 +28,18 @@ import java.util.Map;
 public class PaymentCallbackServiceImpl implements PaymentCallbackService {
     @Autowired
     private BookingOrderManager bookingOrderManager;
+    @Autowired
+    private LockTransactionManager lockTransactionManager;
 
     @Autowired
     private OrderMapper orderMapper;
 
+    @Autowired
+    private DistributedLockTemplate distributedLockTemplate;
+    @Value("${lock.timeout}")
+    private int lockTimeOut = 3;
+
     @Override
-    @Transactional(rollbackFor = {Exception.class, RuntimeException.class})
     public Result payCallback(long ordCode, String serialNum, String outTransId) {
         log.debug("支付回调:订单号:{};流水:{};交易号:{}", new Object[]{ordCode, serialNum, outTransId});
 
@@ -40,17 +49,25 @@ public class PaymentCallbackServiceImpl implements PaymentCallbackService {
             return new Result(false, "订单不存在");
         }
 
-        // 支付成功回调 记录任务
-        Map<String, String> jobParams = new HashMap<>();
-        jobParams.put("orderId", order.getId().toString());
-        jobParams.put("outTransId", outTransId);
-        jobParams.put("serialNum", serialNum);
-        bookingOrderManager.addJob(OrderConstant.Actions.PAYMENT_CALLBACK, jobParams);
+        DistributedReentrantLock lock = distributedLockTemplate.execute(String.valueOf(order.getId()), lockTimeOut);
+
+        try {
+            lockTransactionManager.paymentCallback(order.getId(), outTransId, serialNum);
+        } catch (Exception e) {
+            log.warn("支付回调异常,添加任务重试", e);
+            // 支付成功回调 记录任务
+            Map<String, String> jobParams = new HashMap<>();
+            jobParams.put("orderId", order.getId().toString());
+            jobParams.put("outTransId", outTransId);
+            jobParams.put("serialNum", serialNum);
+            bookingOrderManager.addJob(OrderConstant.Actions.PAYMENT_CALLBACK, jobParams);
+        } finally {
+            lock.unlock();
+        }
         return new Result(true);
     }
 
     @Override
-    @Transactional(rollbackFor = {Exception.class, RuntimeException.class})
     public Result refundCallback(Long ordCode, String serialNum, String outTransId, boolean success) {
         log.debug("退款回调-{}:订单号:{};流水:{};交易号:{}", new Object[]{success, ordCode, serialNum, outTransId});
         Order order = orderMapper.selectBySN(ordCode);
@@ -66,13 +83,22 @@ public class PaymentCallbackServiceImpl implements PaymentCallbackService {
             return new Result(false, "订单号不存在");
         }
 
-        // 退款成功回调 记录任务
-        Map<String, String> jobParams = new HashMap<>();
-        jobParams.put("orderId", order.getId().toString());
-        jobParams.put("ordCode", String.valueOf(ordCode));
-        jobParams.put("serialNum", serialNum);
-        jobParams.put("success", Boolean.toString(success));
-        bookingOrderManager.addJob(OrderConstant.Actions.REFUND_MONEY_CALLBACK, jobParams);
+        DistributedReentrantLock lock = distributedLockTemplate.execute(String.valueOf(order.getId()), lockTimeOut);
+
+        try {
+            lockTransactionManager.refundMoneyCallback(order.getId(), ordCode, serialNum, success);
+        } catch (Exception e) {
+            log.warn("退款回调异常,添加任务重试", e);
+            // 退款成功回调 记录任务
+            Map<String, String> jobParams = new HashMap<>();
+            jobParams.put("orderId", order.getId().toString());
+            jobParams.put("ordCode", String.valueOf(ordCode));
+            jobParams.put("serialNum", serialNum);
+            jobParams.put("success", Boolean.toString(success));
+            bookingOrderManager.addJob(OrderConstant.Actions.REFUND_MONEY_CALLBACK, jobParams);
+        } finally {
+            lock.unlock();
+        }
 
         return new Result(true);
     }
