147dd4f7787c756e8bb1ef4f819fc8625b5e2c6d
wangximing <wangxm@all580.com>
2017-02-18 14:53:59 +0800

    退票数 只算退票成功的

--- order-api/src/main/java/com/all580/order/api/service/event/ConsumeTicketSyncDataEvent.java
+++ order-api/src/main/java/com/all580/order/api/service/event/ConsumeTicketSyncDataEvent.java
@@ -1,6 +1,8 @@
 package com.all580.order.api.service.event;
 
+import com.all580.order.api.OrderConstant;
 import com.all580.order.api.model.ConsumeTicketEventParam;
+import com.framework.common.event.EventService;
 import com.framework.common.mns.MnsSubscribeAction;
 
 /**
@@ -9,5 +11,6 @@ import com.framework.common.mns.MnsSubscribeAction;
  * @Description: 核销票据同步数据事件
  * @date 2017/2/9 11:48
  */
+@EventService(OrderConstant.EventType.CONSUME_TICKET)
 public interface ConsumeTicketSyncDataEvent extends MnsSubscribeAction<ConsumeTicketEventParam> {
 }
