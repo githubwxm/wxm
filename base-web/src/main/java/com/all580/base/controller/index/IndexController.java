4c71334fd6b9d5f3ae20033e26c995e5479497ce
wangximing <wangxm@all580.com>
2017-02-18 15:42:41 +0800

    添加是否发送短信

--- base-web/src/main/java/com/all580/base/controller/index/IndexController.java
+++ base-web/src/main/java/com/all580/base/controller/index/IndexController.java
@@ -1,19 +1,19 @@
 package com.all580.base.controller.index;
 
+import com.all580.notice.api.service.SmsService;
+import com.framework.common.BaseController;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import com.framework.common.BaseController;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class IndexController extends BaseController {
-	
-	
-	
-	@RequestMapping("/")
-	public String homePage(){
-	
-		return "index";
-	}
+	@Autowired
+	private SmsService smsService;
 
+	@RequestMapping("/sms/set")
+	public void setSms(@RequestParam boolean is){
+		smsService.setIsSend(is);
+	}
 }
