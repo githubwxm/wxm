3de6427685b29acda991cdf66556c4d810191a22
wangximing <wangxm@all580.com>
2017-02-28 11:18:04 +0800

    添加审核通知事件

---
+++ order-rpc/src/main/java/com/all580/order/service/event/RefundAuditNotifyEventImpl.java
@@ -0,0 +1,47 @@
+package com.all580.order.service.event;
+
+
+import com.all580.order.api.model.RefundAuditEventParam;
+import com.all580.order.api.service.event.RefundAuditNotifyEvent;
+import com.all580.order.dao.OrderMapper;
+import com.all580.order.dao.RefundOrderMapper;
+import com.all580.order.entity.Order;
+import com.all580.order.entity.RefundOrder;
+import com.framework.common.Result;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by wxming on 2017/2/28 0028.
+ */
+@Slf4j
+@Service
+public class RefundAuditNotifyEventImpl  extends BaseNotifyEvent implements RefundAuditNotifyEvent {
+    @Autowired
+    private RefundOrderMapper refundOrderMapper;
+
+    @Autowired
+    private OrderMapper orderMapper;
+    @Override
+    public Result process(String s, RefundAuditEventParam content, Date date) {
+        RefundOrder refundOrder = refundOrderMapper.selectByPrimaryKey(content.getRefundId());
+        Assert.notNull(refundOrder, "退订订单不存在");
+        String serialNo=refundOrder.getOuter_id();
+        if (!content.isStatus()) {
+            Assert.notNull(refundOrder, "退订订单不存在");
+            Map<String,Object> map = new HashMap<>();
+            map.put("refund_seq",serialNo);
+            map.put("refund_result","REJECT");
+            map.put("refund_quantity",refundOrder.getQuantity());
+            log.info("退订审核不通过 Order_item_id: {} " ,refundOrder.getOrder_item_id());
+            notifyEvent(refundOrder.getOrder_item_id(), "REFUND",map);
+        }
+        return new Result(true);
+    }
+}
