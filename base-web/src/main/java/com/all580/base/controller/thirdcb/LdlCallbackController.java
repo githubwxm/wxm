0a0080b344efaad0dc020e1162d96d82e17a4a18
zq <zmjq08@gmail.com>
2017-03-02 16:53:56 +0800

    增加旅动力回调网关接口

---
+++ base-web/src/main/java/com/all580/base/controller/thirdcb/LdlCallbackController.java
@@ -0,0 +1,57 @@
+package com.all580.base.controller.thirdcb;
+
+import com.all580.voucher.api.service.third.LdlRPCService;
+import com.framework.common.util.CommonUtil;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.Resource;
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+@RequestMapping("thirdcb/ldl")
+public class LdlCallbackController {
+
+    @Resource
+    LdlRPCService ldlService;
+
+    private static final String UPDATE_PRODUCT_CB_ACTION = "product_change";
+
+    private static final String VERIFY_ORDER_CB_ACTION = "order_verify";
+
+    private static final String REFUND_ORDER_CB_ACTION = "order_refund";
+
+    /**
+     *　旅动力回调入口
+     * @param params
+     * @return
+     */
+    @RequestMapping(value = "facade", method = RequestMethod.POST)
+    @ResponseBody
+    public Map<String, Object> facade(@RequestBody Map<String, Object> params) {
+        String action = params.get("action").toString();
+        if (UPDATE_PRODUCT_CB_ACTION.equals(action))
+            ldlService.updateProductCallback(params.get("key").toString(),
+                CommonUtil.objectParseInteger(params.get("client_id")),
+                params.get("action").toString(),
+                (Map<String, Object>) params.get("body"));
+        else if (VERIFY_ORDER_CB_ACTION.equals(action))
+            ldlService.validOrderCallback(params.get("key").toString(),
+                CommonUtil.objectParseInteger(params.get("client_id")),
+                params.get("action").toString(),
+                (Map<String, Object>) params.get("body"));
+        else if (REFUND_ORDER_CB_ACTION.equals(action))
+            ldlService.refundOrderCallback(params.get("key").toString(),
+                CommonUtil.objectParseInteger(params.get("client_id")),
+                params.get("action").toString(),
+                (Map<String, Object>) params.get("body"));
+        Map<String, Object> map = new HashMap<>();
+        map.put("code", 200);
+        map.put("describe", "success");
+        return map;
+    }
+}
