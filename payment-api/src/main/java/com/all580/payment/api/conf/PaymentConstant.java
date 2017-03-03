674129f41964c589416d38eac556c3a063c9403a
wangximing <wangxm@all580.com>
2017-03-03 16:58:46 +0800

    添加支付宝微信图片

--- payment-api/src/main/java/com/all580/payment/api/conf/PaymentConstant.java
+++ payment-api/src/main/java/com/all580/payment/api/conf/PaymentConstant.java
@@ -86,7 +86,7 @@ public class PaymentConstant {
      * 支付方式
      */
     public static class PaymentiImg {
-        public static final String ALI_PAY_IMG = "http://img.all580.cn/star/upls/2017/03/03/58b92caa3f84c.png"; // 支付宝
-        public static final String WX_PAY_IMG = "http://img.all580.cn/star/upls/2017/03/03/58b92ceca296d.png"; // 微信
+        public static final String ALI_PAY_IMG = "star/upls/2017/03/03/58b92caa3f84c.png"; // 支付宝
+        public static final String WX_PAY_IMG = "star/upls/2017/03/03/58b92ceca296d.png"; // 微信
     }
 }
