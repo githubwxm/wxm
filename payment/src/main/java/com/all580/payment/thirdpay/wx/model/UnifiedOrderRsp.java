11fab77c20625def6189f141cbd04c5487ad4b70
wangximing <wangxm@all580.com>
2017-03-06 14:29:01 +0800

    添加支付宝微信图片

--- payment/src/main/java/com/all580/payment/thirdpay/wx/model/UnifiedOrderRsp.java
+++ payment/src/main/java/com/all580/payment/thirdpay/wx/model/UnifiedOrderRsp.java
@@ -1,11 +1,14 @@
 package com.all580.payment.thirdpay.wx.model;
 
+import lombok.Data;
+
 import java.io.Serializable;
 
 /**
  * @author panyi on 2016/10/13.
  * @since V0.0.1
  */
+@Data
 public class UnifiedOrderRsp extends CommonsRsp implements Serializable {
 
     //    公众账号ID	appid	是	String(32)	wx8888888888888888	调用接口提交的公众账号ID
@@ -25,6 +28,7 @@ public class UnifiedOrderRsp extends CommonsRsp implements Serializable {
     //    错误代码描述	err_code_des	否	String(128)	系统错误	错误返回的信息描述
     private String err_code_des;
 
+    private String transaction_id;
     //    交易类型	trade_type	是	String(16)	JSAPI	调用接口提交的交易类型，取值如下：JSAPI，NATIVE，APP，详细说明见参数规定
     private String trade_type;
     //    预支付交易会话标识	prepay_id	是	String(64)	wx201410272009395522657a690389285100	微信生成的预支付回话标识，用于后续接口调用中使用，该值有效期为2小时
@@ -42,118 +46,8 @@ public class UnifiedOrderRsp extends CommonsRsp implements Serializable {
 //    USERPAYING--用户支付中
 //    PAYERROR--支付失败(其他原因，如银行返回失败)
     private String trade_state;
-    public String getTrade_state() {
-        return trade_state;
-    }
 
-    public void setTrade_state(String trade_state) {
-        this.trade_state = trade_state;
-    }
 
-    public String getAppid() {
-        return appid;
-    }
 
-    public void setAppid(String appid) {
-        this.appid = appid;
-    }
 
-    public String getMch_id() {
-        return mch_id;
-    }
-
-    public void setMch_id(String mch_id) {
-        this.mch_id = mch_id;
-    }
-
-    public String getDevice_info() {
-        return device_info;
-    }
-
-    public void setDevice_info(String device_info) {
-        this.device_info = device_info;
-    }
-
-    public String getNonce_str() {
-        return nonce_str;
-    }
-
-    public void setNonce_str(String nonce_str) {
-        this.nonce_str = nonce_str;
-    }
-
-    public String getSign() {
-        return sign;
-    }
-
-    public void setSign(String sign) {
-        this.sign = sign;
-    }
-
-    public String getResult_code() {
-        return result_code;
-    }
-
-    public void setResult_code(String result_code) {
-        this.result_code = result_code;
-    }
-
-    public String getErr_code() {
-        return err_code;
-    }
-
-    public void setErr_code(String err_code) {
-        this.err_code = err_code;
-    }
-
-    public String getErr_code_des() {
-        return err_code_des;
-    }
-
-    public void setErr_code_des(String err_code_des) {
-        this.err_code_des = err_code_des;
-    }
-
-    public String getTrade_type() {
-        return trade_type;
-    }
-
-    public void setTrade_type(String trade_type) {
-        this.trade_type = trade_type;
-    }
-
-    public String getPrepay_id() {
-        return prepay_id;
-    }
-
-    public void setPrepay_id(String prepay_id) {
-        this.prepay_id = prepay_id;
-    }
-
-    public String getCode_url() {
-        return code_url;
-    }
-
-    public void setCode_url(String code_url) {
-        this.code_url = code_url;
-    }
-
-    @Override
-    public String toString() {
-        return "UnifiedOrderRsp [" +
-                "return_code='" + return_code + '\'' +
-                ", return_msg='" + return_msg + '\'' +
-                ", appid='" + appid + '\'' +
-                ", mch_id='" + mch_id + '\'' +
-                ", device_info='" + device_info + '\'' +
-                ", nonce_str='" + nonce_str + '\'' +
-                ", sign='" + sign + '\'' +
-                ", result_code='" + result_code + '\'' +
-                ", err_code='" + err_code + '\'' +
-                ", err_code_des='" + err_code_des + '\'' +
-                ", trade_type='" + trade_type + '\'' +
-                ", prepay_id='" + prepay_id + '\'' +
-                ", code_url='" + code_url + '\'' +
-                ']';
-    }
 }
