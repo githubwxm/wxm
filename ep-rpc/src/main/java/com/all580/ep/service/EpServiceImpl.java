87f8444576d45b43067bc6e6265ad1065deadd61
wangximing <wangxm@all580.com>
2017-03-08 16:02:18 +0800

    优化代码

--- ep-rpc/src/main/java/com/all580/ep/service/EpServiceImpl.java
+++ ep-rpc/src/main/java/com/all580/ep/service/EpServiceImpl.java
@@ -655,11 +655,9 @@ public class EpServiceImpl implements EpService {
     @Override
     public Result<Map<String, Object>> updateEp(Map<String, Object> map) {
         Result<Map<String, Object>> result = new Result<>();
-        try {
-            checkNamePhone(map);//检查电话与名字是否存在 ，存在抛出异常
-        } catch (ApiException e) {
-            log.error(e.getMessage(), e);
-            throw new ApiException(e.getMessage(), e);
+        Result  checkResult= checkNamePhone(map);//检查电话与名字是否存在 ，存在抛出异常
+        if(!checkResult.isSuccess()){
+            return checkResult;
         }
         try {
             //CommonUtil.formtAddress(map);
