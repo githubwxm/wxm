622cc8a878e397a58af1dcf9c39aaa5c5c6b11ee
zxjun <715213471@qq.com>
2017-03-24 11:47:00 +0800

    fix(service): 旅动力已退票成功的重复调用新增错误码 (#14)
    如果不等于退票中状态则返回错误码: 102
    
    Closes #14

--- order-rpc/src/main/java/com/all580/order/service/TicketCallbackServiceImpl.java
+++ order-rpc/src/main/java/com/all580/order/service/TicketCallbackServiceImpl.java
@@ -428,7 +428,9 @@ public class TicketCallbackServiceImpl implements TicketCallbackService {
                 refundOrderResult = refundOrder;
             }
         }
-        Assert.notNull(refundOrderResult, "没有在退订中的退订订单");
+        if (refundOrderResult == null) {
+            throw new ApiException(Result.UNIQUE_KEY_ERROR, "没有在退订中的退订订单");
+        }
         List<Visitor> visitors = visitorMapper.selectByOrderItem(orderItem.getId());
         if (visitors.size() > 1) {
             throw new ApiException("该订单有多个游客");
