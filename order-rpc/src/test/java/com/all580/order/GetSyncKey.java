bf5cfff9da0a4451bad0b13aa507a3296bf72587
zxjun <715213471@qq.com>
2017-03-31 15:33:04 +0800

    fix(service): 超时待支付/待审核自动取消任务需要锁(#26)
    Closes #26

--- order-rpc/src/test/java/com/all580/order/GetSyncKey.java
+++ order-rpc/src/test/java/com/all580/order/GetSyncKey.java
@@ -12,6 +12,7 @@ public class GetSyncKey {
     public static void main(String[] args) {
         System.out.println("畅旅测试环境:" + Md5Utils.getMD5For16("T_1476277250138WFBZ35GTM7PL25").toUpperCase()); // 畅旅
         System.out.println("畅旅生产环境:" + Md5Utils.getMD5For16("P_1476277250138WFBZ35GTM7PL25").toUpperCase()); // 畅旅
+        System.out.println("畅旅演示环境:" + Md5Utils.getMD5For16("V_1476277250138WFBZ35GTM7PL25").toUpperCase()); // 畅旅
         System.out.println("畅旅开发环境:" + Md5Utils.getMD5For16("D_1476277250138WFBZ35GTM7PL25").toUpperCase()); // 畅旅
         System.out.println("策票生产环境:" + Md5Utils.getMD5For16("P_1488267799748EXBSWRREXW77BE").toUpperCase()); // 策票
         System.out.println("德夯生产环境:" + Md5Utils.getMD5For16("P_14901686773782Y8QKCVA3HHN7F").toUpperCase()); // 德夯
