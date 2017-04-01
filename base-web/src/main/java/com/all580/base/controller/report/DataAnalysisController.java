fb6388c2dfe4bb2aca3f444d056c1d96f02233e9
zxjun <715213471@qq.com>
2017-04-01 10:43:35 +0800

    feat(controller): 新增数据分析功能网关(#27)
    获取平台商ID

--- base-web/src/main/java/com/all580/base/controller/report/DataAnalysisController.java
+++ base-web/src/main/java/com/all580/base/controller/report/DataAnalysisController.java
@@ -2,10 +2,12 @@ package com.all580.base.controller.report;
 
 import com.all580.base.controller.payment.BalanceController;
 import com.all580.base.util.Utils;
+import com.all580.ep.api.conf.EpConstant;
 import com.all580.report.api.ReportConstant;
 import com.all580.report.api.service.DataAnalysisService;
 import com.framework.common.Result;
 import com.framework.common.lang.DateFormatUtils;
+import com.framework.common.util.CommonUtil;
 import org.apache.commons.lang3.ArrayUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -35,7 +37,6 @@ public class DataAnalysisController extends BalanceController {
     public Result<?> consumeListBySale(Integer product_id,
                                        @RequestParam String start,
                                        @RequestParam String end,
-                                       @RequestParam Integer core_ep_id,
                                        String sortCol,
                                        String sortType,
                                        @RequestParam(defaultValue = "0") Integer record_start,
@@ -48,7 +49,8 @@ public class DataAnalysisController extends BalanceController {
         if (!ArrayUtils.contains(sortCols, sortCol)) {
             throw new ApiException("排序字段错误");
         }
-        return dataAnalysisService.selectConsumeAnalysisListBySale(product_id, dates[0], dates[1], core_ep_id, sortCol, sortType, record_start, record_count);
+        Object core_ep_id = getAttribute(EpConstant.EpKey.CORE_EP_ID);
+        return dataAnalysisService.selectConsumeAnalysisListBySale(product_id, dates[0], dates[1], CommonUtil.objectParseInteger(core_ep_id), sortCol, sortType, record_start, record_count);
     }
 
     @RequestMapping("sale/consume/compared")
@@ -56,7 +58,6 @@ public class DataAnalysisController extends BalanceController {
     public Result<?> consumeComparedBySale(Integer product_id,
                                            @RequestParam String start,
                                            @RequestParam String end,
-                                           @RequestParam Integer core_ep_id,
                                            @RequestParam Integer type,
                                            @RequestParam(defaultValue = "false") boolean group,
                                            String sortCol,
@@ -75,7 +76,8 @@ public class DataAnalysisController extends BalanceController {
         if (!ArrayUtils.contains(types, type)) {
             throw new ApiException("类型错误");
         }
-        return dataAnalysisService.selectConsumeAnalysisComparedSale(product_id, dates[0], dates[1], core_ep_id, type, group ? "team" : "individual", sortCol, sortType, record_start, record_count);
+        Object core_ep_id = getAttribute(EpConstant.EpKey.CORE_EP_ID);
+        return dataAnalysisService.selectConsumeAnalysisComparedSale(product_id, dates[0], dates[1], CommonUtil.objectParseInteger(core_ep_id), type, group ? "team" : "individual", sortCol, sortType, record_start, record_count);
     }
 
     public static Date[] checkDate(String start_time, String end_time) {
