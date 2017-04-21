1834294f35068a87c7f41227f765d9345a959352
zxjun <zhouxianjun@all580.com>
2017-04-21 15:36:11 +0800

    feat(order-rpc): 订单日志支付和取消事件重叠

--- order-rpc/src/main/java/com/all580/order/service/event/PaidSyncDataEventImpl.java
+++ order-rpc/src/main/java/com/all580/order/service/event/PaidSyncDataEventImpl.java
@@ -1,7 +1,16 @@
 package com.all580.order.service.event;
 
 import com.all580.order.api.service.event.PaidSyncDataEvent;
+import com.all580.order.dao.OrderItemMapper;
+import com.all580.order.dao.OrderMapper;
+import com.all580.order.dto.SyncAccess;
+import com.all580.order.entity.Order;
+import com.framework.common.Result;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
+
+import java.util.Date;
 
 /**
  * @author zhouxianjun(Alone)
@@ -10,6 +19,24 @@ import org.springframework.stereotype.Service;
  * @date 2017/1/24 15:44
  */
 @Service
-public class PaidSyncDataEventImpl extends OrderCancelSyncDataEventImpl implements PaidSyncDataEvent {
+public class PaidSyncDataEventImpl extends BasicSyncDataEvent implements PaidSyncDataEvent {
+    @Autowired
+    private OrderMapper orderMapper;
+    @Autowired
+    private OrderItemMapper orderItemMapper;
 
+    @Override
+    public Result process(String s, Integer integer, Date date) {
+        Order order = orderMapper.selectByPrimaryKey(integer);
+        Assert.notNull(order);
+
+        SyncAccess syncAccess = getAccessKeys(order);
+        syncAccess.getDataMap()
+                .add("t_order", order)
+                .add("t_order_item", orderItemMapper.selectByOrderId(order.getId()));
+
+        syncAccess.loop();
+        sync(syncAccess.getDataMaps());
+        return new Result(true);
+    }
 }
