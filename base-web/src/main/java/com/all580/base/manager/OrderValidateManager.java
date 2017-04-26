4dd8e231562c3881a39d1c781f41e1a696794550
zxjun <zhouxianjun@all580.com>
2017-04-26 17:34:37 +0800

    fix(order-rpc): 修改团队新增cardtype

--- base-web/src/main/java/com/all580/base/manager/OrderValidateManager.java
+++ base-web/src/main/java/com/all580/base/manager/OrderValidateManager.java
@@ -1,5 +1,6 @@
 package com.all580.base.manager;
 
+import com.all580.order.api.OrderConstant;
 import com.all580.product.api.consts.ProductConstants;
 import com.framework.common.validate.ValidRule;
 import org.springframework.stereotype.Component;
@@ -382,6 +383,9 @@ public class OrderValidateManager {
                 "guide_sid",
                 "visitors.sid"
         }, new ValidRule[]{new ValidRule.IdCard()});
+        rules.put(new String[]{
+                "visitors.card_type" // 证件类型
+        }, new ValidRule[]{new ValidRule.Digits(new Long[]{Long.valueOf(OrderConstant.CardType.ID), Long.valueOf(OrderConstant.CardType.OTHER)})});
         return rules;
     }
 
