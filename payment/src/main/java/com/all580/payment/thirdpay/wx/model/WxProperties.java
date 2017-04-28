ae803974f7bc2fd952a3ceb2f2073dd9df7243ce
zxjun <zhouxianjun@all580.com>
2017-04-28 11:38:09 +0800

    feat(payment): 微信token从网站获取(#47)
    Closes #47

--- payment/src/main/java/com/all580/payment/thirdpay/wx/model/WxProperties.java
+++ payment/src/main/java/com/all580/payment/thirdpay/wx/model/WxProperties.java
@@ -1,11 +1,14 @@
 package com.all580.payment.thirdpay.wx.model;
 
+import lombok.*;
+
 /**
  * 微信账号配置信息
  *
  * @author panyi on 2016/10/13.
  * @since V0.0.1
  */
+@Data
 public class WxProperties {
     private String api_client_cert_p12_str; // 私钥证书文件字符串
     private String app_id; // 开发应用ID
@@ -15,52 +18,6 @@ public class WxProperties {
 
     private int core_ep_id; // 平台商ID
     //private
-
-    public String getApp_secret() {
-        return app_secret;
-    }
-
-    public void setApp_secret(String app_secret) {
-        this.app_secret = app_secret;
-    }
-
-    public String getApi_client_cert_p12_str() {
-        return api_client_cert_p12_str;
-    }
-
-    public void setApi_client_cert_p12_str(String api_client_cert_p12_str) {
-        this.api_client_cert_p12_str = api_client_cert_p12_str;
-    }
-
-    public String getApp_id() {
-        return app_id;
-    }
-
-    public void setApp_id(String app_id) {
-        this.app_id = app_id;
-    }
-
-    public String getMch_id() {
-        return mch_id;
-    }
-
-    public void setMch_id(String mch_id) {
-        this.mch_id = mch_id;
-    }
-
-    public String getMch_key() {
-        return mch_key;
-    }
-
-    public void setMch_key(String mch_key) {
-        this.mch_key = mch_key;
-    }
-
-    public int getCore_ep_id() {
-        return core_ep_id;
-    }
-
-    public void setCore_ep_id(int core_ep_id) {
-        this.core_ep_id = core_ep_id;
-    }
+    private String web_access_key; //网站access_key
+    private String web_access_id; //网站access_id
 }
