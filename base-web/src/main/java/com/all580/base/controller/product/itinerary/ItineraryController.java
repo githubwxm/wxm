b69eed34634a433b3a570a6fe92d49f8969e877a
wangximing <wangxm@all580.com>
2017-05-18 16:17:42 +0800

    fix(同步数据) : 同步数据分页500(#38)

---
+++ base-web/src/main/java/com/all580/base/controller/product/itinerary/ItineraryController.java
@@ -0,0 +1,12 @@
+package com.all580.base.controller.product.itinerary;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by wxming on 2017/5/18 0018.
+ */
+@Controller
+@RequestMapping(value = "api/product/itinerary")
+public class ItineraryController {
+}
