3a92cdf9249a572284f2f8d64fd57e27968a41ba
zq <zmjq08@gmail.com>
2017-05-18 10:43:34 +0800

    feat(票付通回调网关): 增加票付通回调网关接口

---
+++ base-web/src/main/java/com/all580/base/controller/thirdcb/PftCallbackController.java
@@ -0,0 +1,23 @@
+package com.all580.base.controller.thirdcb;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Map;
+
+@Slf4j
+@Controller
+@RequestMapping("thirdcb/pft")
+public class PftCallbackController {
+
+    @RequestMapping(value = "notify", method = RequestMethod.POST)
+    @ResponseBody
+    public String notify(@RequestBody Map<String, Object> params) {
+        return "success";
+    }
+}
