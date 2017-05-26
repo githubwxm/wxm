476ce5a5578de1c826150e3b03504856185d1f9a
wangximing <wangxm@all580.com>
2017-05-26 09:26:20 +0800

    feat(web_base): 线路起始地，目的地修改(#15)

---
+++ base-web/src/main/java/com/all580/base/controller/product/itinerary/ItineraryBatchController.java
@@ -0,0 +1,12 @@
+package com.all580.base.controller.product.itinerary;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by wxming on 2017/5/25 0025.
+ */
+@Controller
+@RequestMapping(value = "api/product/itinerary/batch")
+public class ItineraryBatchController {
+}
