28da8b4f0fc3d21a38f4a289128ad0074ab3202f
wangximing <wangxm@all580.com>
2017-07-10 15:54:48 +0800

    feat(web_base):    异常订单按照  平台商与 销售商来查询     (#15)

--- base-web/src/main/java/com/all580/base/controller/order/OrderExceptionContoller.java
+++ base-web/src/main/java/com/all580/base/controller/order/OrderExceptionContoller.java
@@ -1,6 +1,8 @@
 package com.all580.base.controller.order;
 
+import com.all580.ep.api.conf.EpConstant;
 import com.all580.order.api.service.SyncExceptionOrder;
+import com.framework.common.BaseController;
 import com.framework.common.Result;
 import com.framework.common.util.CommonUtil;
 import lombok.extern.slf4j.Slf4j;
@@ -9,7 +11,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Date;
+import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -19,17 +21,18 @@ import java.util.Map;
 @Controller
 @RequestMapping("api/exception/order")
 @Slf4j
-public class OrderExceptionContoller {
+public class OrderExceptionContoller extends BaseController {
     @Autowired
     private SyncExceptionOrder syncExceptionOrder;
 
     @RequestMapping(value = "exception/list")
     @ResponseBody
-    public Result<?> selectOrderException( String supplier_ep_name,
-                                            String buy_ep_name,
-                                           Long number,String start,String end,
-                                           @RequestParam(defaultValue = "0") Integer record_start,
-                                           @RequestParam(defaultValue = "20") Integer record_count) {
+    public Result<?> selectOrderException(HttpServletRequest request, Integer ep_id, String supplier_ep_name,
+                                          String buy_ep_name,
+                                          Long number, String start, String end,
+                                          @RequestParam(defaultValue = "0") Integer record_start,
+                                          @RequestParam(defaultValue = "20") Integer record_count) {
+        Integer core_ep_id =CommonUtil.objectParseInteger(request.getAttribute(EpConstant.EpKey.CORE_EP_ID)) ;
         Map<String,Object> map = new HashMap<>();
         map.put("supplier_ep_name",supplier_ep_name);
         map.put("buy_ep_name",buy_ep_name);
@@ -38,6 +41,11 @@ public class OrderExceptionContoller {
         map.put("end",end);
         map.put("record_start",record_start);
         map.put("record_count",record_count);
+        if(core_ep_id-ep_id==0){
+           map.put("core_ep_id",core_ep_id);
+        }else{
+           map.put("buy_ep_id",ep_id);
+        }
         return syncExceptionOrder.selectOrderException(map);
     }
     @RequestMapping(value = "select/sync")
