e3af0b5a2f239f87a5ee4c62fa2a363c628774a6
wangximing <wangxm@all580.com>
2017-08-08 09:52:36 +0800

    fix(test)  添加日志测试

--- order-rpc/src/main/java/com/all580/order/service/event/OrderAuditNotifyEventImpl.java
+++ order-rpc/src/main/java/com/all580/order/service/event/OrderAuditNotifyEventImpl.java
@@ -1,9 +1,9 @@
 package com.all580.order.service.event;
 
 import com.all580.order.api.model.OrderAuditEventParam;
-import com.all580.order.api.service.event.CancelTicketNotifyEvent;
 import com.all580.order.api.service.event.OrderAuditNotifyEvent;
 import com.framework.common.Result;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
@@ -11,10 +11,12 @@ import java.util.Date;
 /**
  * Created by wxming on 2017/2/16 0016.
  */
+@Slf4j
 @Service
 public class OrderAuditNotifyEventImpl  extends BaseNotifyEvent implements OrderAuditNotifyEvent {
     @Override
     public Result process(String s, OrderAuditEventParam orderAuditEventParam, Date date) {
+        log.error("查看审核状态"+orderAuditEventParam.toString());
         notifyEvent(  orderAuditEventParam.getItemId(), "ORTHER",null);
         return new Result(true);
     }
