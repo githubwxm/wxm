2d96835426470d2b7c0b70b81bd41eb3e004bf35
wangximing <wangxm@all580.com>
2017-08-18 09:53:05 +0800

    feat(controller)  添加 文件名称  与  区分报表名称

--- base-web/src/test/java/com/all580/base/controller/com/all580/base/order/Task.java
+++ base-web/src/test/java/com/all580/base/controller/com/all580/base/order/Task.java
@@ -1,7 +1,15 @@
 package com.all580.base.controller.com.all580.base.order;
 
+import com.framework.common.lang.JsonUtils;
+import com.framework.common.net.HttpUtils;
+import com.framework.common.util.CommonUtil;
+
 import java.util.Date;
 import java.util.LinkedList;
+import java.util.Map;
+import java.util.TreeMap;
+
+import static com.all580.base.controller.com.all580.base.order.CreateOrder.url;
 
 /**
  * Created by wxming on 2017/7/31 0031.
@@ -25,9 +33,27 @@ public class Task implements Runnable {
     {
         while (!linkedList.isEmpty())
         {
-            CreateOrder.start(Linked.getOrderTicket(),CreateOrder.url,linkedList.size());
+            CreateOrder.start(Linked.getOrderTicket(), url,linkedList.size());
         }
         System.out.println("linkedList 为空  end:"+new Date());
         CollectSql.addSql("end:"+new Date());
+    }
+    public static void main(String a[]){
+        String url ="http://service.all580.com.cn/api/order/refund/audit";
+        String json = "{\"refund_sn\":\"1502716122755780\", \"status\": \"true\", \"reason\":\"ok\"}";
+        Map<String, Object> params = JsonUtils.json2Map(json);
+        params.put("access_id","1476277249859N2T3JBGA");
+        params.put("ep_id","1");
+        params.put("operator_name","畅旅");
+        params.put("operator_id","1");
+        TreeMap tree=new TreeMap(params);
+        String  postParams = JsonUtils.toJson(tree);
+        postParams=postParams.replace("null","");
+        //['"',"\\","[","]","{","}",'null'
+        postParams=postParams.replaceAll("[\"\\\\\\[\\]\\{\\}]","");
+        String resultSign= CommonUtil.signForData("1476277250138WFBZ35GTM7PL25",postParams);
+        params.put("sign",resultSign);
+        String response = HttpUtils.postJson(url, JsonUtils.toJson(params), "UTF-8");
+        System.out.println(response);
     }
 }
