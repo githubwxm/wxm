3bf352da616860dce6c38e15aee5713592b7218b
wangximing <wangxm@all580.com>
2017-08-29 10:21:16 +0800

    feat(service)   审核通过不通过添加 支付方式

--- order-rpc/src/main/java/com/all580/order/service/event/OrderAuditNotifyEventImpl.java
+++ order-rpc/src/main/java/com/all580/order/service/event/OrderAuditNotifyEventImpl.java
@@ -2,11 +2,17 @@ package com.all580.order.service.event;
 
 import com.all580.order.api.model.OrderAuditEventParam;
 import com.all580.order.api.service.event.OrderAuditNotifyEvent;
+import com.all580.order.dao.OrderItemMapper;
+import com.all580.order.entity.OrderItem;
 import com.framework.common.Result;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by wxming on 2017/2/16 0016.
@@ -14,15 +20,23 @@ import java.util.Date;
 @Slf4j
 @Service
 public class OrderAuditNotifyEventImpl  extends BaseNotifyEvent implements OrderAuditNotifyEvent {
+
+
+    @Autowired
+    private OrderItemMapper orderItemMapper;
     @Override
     public Result process(String s, OrderAuditEventParam orderAuditEventParam, Date date) {
         String op_code="ORTHER";
+        OrderItem item = orderItemMapper.selectByPrimaryKey(orderAuditEventParam.getItemId());
+        Assert.notNull(item);
         if(orderAuditEventParam.isStatus()){
             op_code="AUDIT_SUCCESS";
         }else{
             op_code="AUDIT_FAIL";
         }
-        notifyEvent(  orderAuditEventParam.getItemId(), op_code,null);
+        Map<String,Object> map = new HashMap<>();
+        map.put("payment_flag",item.getPayment_flag());
+        notifyEvent(  orderAuditEventParam.getItemId(), op_code,map);
         return new Result(true);
     }
 }
