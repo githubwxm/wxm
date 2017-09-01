bd078cac7627d12de077e78384610cd56b8358c7
wangximing <wangxm@all580.com>
2017-09-01 16:08:59 +0800

    fix(报表)  报表合到master

--- base-web/src/main/java/com/all580/base/controller/report/ReportExportTaskController.java
+++ base-web/src/main/java/com/all580/base/controller/report/ReportExportTaskController.java
@@ -7,8 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -41,6 +44,18 @@ public class ReportExportTaskController {
         map.put("record_count",record_count);
         return  reportExportTaskService.selectExportTask(map);
     }
+    @RequestMapping(value = "task/down", method = RequestMethod.GET)
+    public String downFile(HttpServletRequest request, HttpServletResponse response, @RequestParam("id") int id){
 
+        Result  result= reportExportTaskService.downFile(id);
+        if(result.isSuccess()){
+            String url =(String) result.get();
+            //return  new ModelAndView(new RedirectView(url));
+            return "redirect:"+url;
+        }else{
+            reportExportTaskService.updateStart(id);
+            return null;
+        }
+    }
 
 }
