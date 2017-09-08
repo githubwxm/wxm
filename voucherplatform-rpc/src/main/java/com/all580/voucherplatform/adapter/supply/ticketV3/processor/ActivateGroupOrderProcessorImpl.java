8a64853bbc2956648a85da06449b4e6cb650bd2c
wangximing <wangxm@all580.com>
2017-09-08 15:25:01 +0800

    fix(voucherplatform)  团队票核销  添加分布式锁

--- voucherplatform-rpc/src/main/java/com/all580/voucherplatform/adapter/supply/ticketV3/processor/ActivateGroupOrderProcessorImpl.java
+++ voucherplatform-rpc/src/main/java/com/all580/voucherplatform/adapter/supply/ticketV3/processor/ActivateGroupOrderProcessorImpl.java
@@ -4,10 +4,13 @@ import com.all580.voucherplatform.adapter.AdapterLoader;
 import com.all580.voucherplatform.adapter.ProcessorService;
 import com.all580.voucherplatform.entity.Supply;
 import com.all580.voucherplatform.manager.order.grouporder.ConsumeGroupOrderManager;
+import com.framework.common.distributed.lock.DistributedLockTemplate;
+import com.framework.common.distributed.lock.DistributedReentrantLock;
 import com.framework.common.util.CommonUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.lang.exception.ApiException;
@@ -24,13 +27,18 @@ public class ActivateGroupOrderProcessorImpl implements ProcessorService<Supply>
     @Autowired
     private AdapterLoader adapterLoader;
 
+    @Autowired
+    private DistributedLockTemplate distributedLockTemplate;
+    @Value("${lock.timeout}")
+    private int lockTimeOut = 3;
+
     @Override
     public Object processor(Supply supply, Map map) {
         String voucherId = CommonUtil.objectParseString(map.get("voucherId"));
         String idNumbers = CommonUtil.objectParseString(map.get("idNumbers"));
         Map mapProd = (Map) map.get("product");
         Integer number = CommonUtil.objectParseInteger(mapProd.get("number"));
-
+        DistributedReentrantLock lock = distributedLockTemplate.execute(voucherId, lockTimeOut);
         ConsumeGroupOrderManager consumeGroupOrderManager = adapterLoader.getBean(ConsumeGroupOrderManager.class);
         consumeGroupOrderManager.setOrder(voucherId);
         try {
@@ -39,9 +47,10 @@ public class ActivateGroupOrderProcessorImpl implements ProcessorService<Supply>
             }else{
                 consumeGroupOrderManager.submiConsume(number, StringUtils.split(idNumbers, ","));
             }
-
         } catch (Exception ex) {
             throw new ApiException(ex);
+        }finally {
+            lock.unlock();
         }
 
         return null;
