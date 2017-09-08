d962f29076e79f64337a5d9c7e8abb079d79d8c6
wangximing <wangxm@all580.com>
2017-09-08 15:55:19 +0800

    fix(报表)  平台供应商 报表查询

--- base-web/src/main/java/com/all580/base/controller/report/OrderSupplierController.java
+++ base-web/src/main/java/com/all580/base/controller/report/OrderSupplierController.java
@@ -5,6 +5,7 @@ import com.all580.product.api.consts.ProductConstants;
 import com.all580.report.api.service.EpOrderService;
 import com.framework.common.BaseController;
 import com.framework.common.Result;
+import com.framework.common.util.CommonUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,172 +28,190 @@ public class OrderSupplierController extends BaseController {
 
     @RequestMapping(value = "supplier/reserve", method = RequestMethod.GET)
     @ResponseBody
-    public Result selectSupplierOrderReserve(Integer ep_id,Integer pro_type,
+    public Result selectSupplierOrderReserve(Integer ep_type, String ep_name, Integer ep_id, Integer pro_type,
                                              @RequestParam("statistic_ep_id") Integer statistic_ep_id,
                                              @RequestParam("statistics_type_id") String statistics_type_id,
-                                             String start  ,String end,String pro_name,Integer group_id,
-                                             String filename,String export_name,
-                                             Integer record_start,Integer record_count){
-        Map<String,Object> map = new HashMap<>();
-        if(null==pro_type){
-            pro_type= ProductConstants.ProductType.SCENERY;
+                                             String start, String end, String pro_name, Integer group_id,
+                                             String filename, String export_name,
+                                             Integer record_start, Integer record_count) {
+        Map<String, Object> map = new HashMap<>();
+        if (null == pro_type) {
+            pro_type = ProductConstants.ProductType.SCENERY;
         }
-        map.put("ep_id",ep_id);
-        map.put("pro_type",pro_type);
-        map.put("statistic_ep_id",statistic_ep_id);
-        map.put("key",statistics_type_id);
-        map.put("start",start);
-        map.put("end",end);
-        map.put("group",group_id);
-        map.put("pro_name",pro_name);
-        map.put("export_name",export_name);
-        map.put("filename",filename);
-        map.put("record_start",record_start);
-        map.put("record_count",record_count);
-        map.put( EpConstant.EpKey.CORE_EP_ID,getAttribute( EpConstant.EpKey.CORE_EP_ID));
-        return  epOrderService.selectSupplierOrderReserve(map);
+        map.put("ep_id", ep_id);
+        map.put("pro_type", pro_type);
+        map.put("statistic_ep_id", statistic_ep_id);
+        map.put("key", statistics_type_id);
+        map.put("start", start);
+        map.put("end", end);
+        map.put("group", group_id);
+        map.put("pro_name", pro_name);
+        map.put("export_name", export_name);
+        map.put("filename", filename);
+        map.put("record_start", record_start);
+        map.put("record_count", record_count);
+        map.put(EpConstant.EpKey.CORE_EP_ID, getAttribute(EpConstant.EpKey.CORE_EP_ID));
+        disposeSupplierPlatform(ep_type,ep_name,map);
+        return epOrderService.selectSupplierOrderReserve(map);
     }
 
     @RequestMapping(value = "supplier/reserve/detail", method = RequestMethod.GET)
     @ResponseBody
-    public Result selectSupplierOrderReserveDetail(Integer ep_id,Integer pro_type,
+    public Result selectSupplierOrderReserveDetail(Integer ep_type, String ep_name,Integer ep_id, Integer pro_type,
                                                    @RequestParam("statistic_ep_id") Integer statistic_ep_id,
-                                                   String start  ,String end,Integer group_id,
-                                                   String date_time  ,String condition,String pro_name,
-                                                   String filename,String export_name,
-                                                   Integer record_start,Integer record_count){
-        Map<String,Object> map = new HashMap<>();
-        if(null==pro_type){
-            pro_type= ProductConstants.ProductType.SCENERY;
+                                                   String start, String end, Integer group_id,
+                                                   String date_time, String condition, String pro_name,
+                                                   String filename, String export_name,
+                                                   Integer record_start, Integer record_count) {
+        Map<String, Object> map = new HashMap<>();
+        if (null == pro_type) {
+            pro_type = ProductConstants.ProductType.SCENERY;
         }
-        map.put("ep_id",ep_id);
-        map.put("group",group_id);
-        map.put("pro_type",pro_type);
-        map.put("statistic_ep_id",statistic_ep_id);
-        map.put("date_time",date_time);
-        map.put("condition",condition);
-        map.put("pro_name",pro_name);
-        map.put("start",start);
-        map.put("end",end);
-        map.put("export_name",export_name);
-        map.put("filename",filename);
-        map.put("record_start",record_start);
-        map.put("record_count",record_count);
-        map.put( EpConstant.EpKey.CORE_EP_ID,getAttribute( EpConstant.EpKey.CORE_EP_ID));
-        return  epOrderService.selectSupplierOrderReserveDetail(map);
+        map.put("ep_id", ep_id);
+        map.put("group", group_id);
+        map.put("pro_type", pro_type);
+        map.put("statistic_ep_id", statistic_ep_id);
+        map.put("date_time", date_time);
+        map.put("condition", condition);
+        map.put("pro_name", pro_name);
+        map.put("start", start);
+        map.put("end", end);
+        map.put("export_name", export_name);
+        map.put("filename", filename);
+        map.put("record_start", record_start);
+        map.put("record_count", record_count);
+        map.put(EpConstant.EpKey.CORE_EP_ID, getAttribute(EpConstant.EpKey.CORE_EP_ID));
+        disposeSupplierPlatform(ep_type,ep_name,map);
+        return epOrderService.selectSupplierOrderReserveDetail(map);
     }
 
     @RequestMapping(value = "supplier/consume", method = RequestMethod.GET)
     @ResponseBody
-    public Result selectSupplierOrderConsume(Integer ep_id,Integer pro_type,Integer group_id,
+    public Result selectSupplierOrderConsume(Integer ep_type, String ep_name,Integer ep_id, Integer pro_type, Integer group_id,
                                              @RequestParam("statistic_ep_id") Integer statistic_ep_id,
                                              @RequestParam("statistics_type_id") String statistics_type_id,
-                                             String start  ,String end,String pro_name,
-                                             String filename,String export_name,
-                                             Integer record_start,Integer record_count){
-        Map<String,Object> map = new HashMap<>();
-        if(null==pro_type){
-            pro_type= ProductConstants.ProductType.SCENERY;
+                                             String start, String end, String pro_name,
+                                             String filename, String export_name,
+                                             Integer record_start, Integer record_count) {
+        Map<String, Object> map = new HashMap<>();
+        if (null == pro_type) {
+            pro_type = ProductConstants.ProductType.SCENERY;
         }
-        map.put("ep_id",ep_id);
-        map.put("group",group_id);
-        map.put("pro_type",pro_type);
-        map.put("statistic_ep_id",statistic_ep_id);
-        map.put("key",statistics_type_id);
-        map.put("start",start);
-        map.put("end",end);
-        map.put("pro_name",pro_name);
-        map.put("export_name",export_name);
-        map.put("filename",filename);
-        map.put("record_start",record_start);
-        map.put("record_count",record_count);
-        map.put( EpConstant.EpKey.CORE_EP_ID,getAttribute( EpConstant.EpKey.CORE_EP_ID));
-        return  epOrderService.selectSupplierOrderConsume(map);
+        map.put("ep_id", ep_id);
+        map.put("group", group_id);
+        map.put("pro_type", pro_type);
+        map.put("statistic_ep_id", statistic_ep_id);
+        map.put("key", statistics_type_id);
+        map.put("start", start);
+        map.put("end", end);
+        map.put("pro_name", pro_name);
+        map.put("export_name", export_name);
+        map.put("filename", filename);
+        map.put("record_start", record_start);
+        map.put("record_count", record_count);
+        map.put(EpConstant.EpKey.CORE_EP_ID, getAttribute(EpConstant.EpKey.CORE_EP_ID));
+        disposeSupplierPlatform(ep_type,ep_name,map);
+        return epOrderService.selectSupplierOrderConsume(map);
     }
 
     @RequestMapping(value = "supplier/consume/detail", method = RequestMethod.GET)
     @ResponseBody
-    public Result selectSupplierOrderConsumeDetail(Integer ep_id,Integer pro_type,
+    public Result selectSupplierOrderConsumeDetail(Integer ep_type, String ep_name,Integer ep_id, Integer pro_type,
                                                    @RequestParam("statistic_ep_id") Integer statistic_ep_id,
-                                                   String start  ,String end,Integer group_id,
-                                                   String date_time  ,String condition,String pro_name,
-                                                   String export_name,String filename,
-                                                   Integer record_start,Integer record_count){
-        Map<String,Object> map = new HashMap<>();
-        if(null==pro_type){
-            pro_type= ProductConstants.ProductType.SCENERY;
+                                                   String start, String end, Integer group_id,
+                                                   String date_time, String condition, String pro_name,
+                                                   String export_name, String filename,
+                                                   Integer record_start, Integer record_count) {
+        Map<String, Object> map = new HashMap<>();
+        if (null == pro_type) {
+            pro_type = ProductConstants.ProductType.SCENERY;
         }
-        map.put("ep_id",ep_id);
-        map.put("group",group_id);
-        map.put("pro_type",pro_type);
-        map.put("statistic_ep_id",statistic_ep_id);
-        map.put("date_time",date_time);
-        map.put("condition",condition);
-        map.put("pro_name",pro_name);
-        map.put("start",start);
-        map.put("end",end);
-        map.put("export_name",export_name);
-        map.put("filename",filename);
-        map.put("record_start",record_start);
-        map.put("record_count",record_count);
-        map.put( EpConstant.EpKey.CORE_EP_ID,getAttribute( EpConstant.EpKey.CORE_EP_ID));
-        return  epOrderService.selectSupplierOrderConsumeDetail(map);
+        map.put("ep_id", ep_id);
+        map.put("group", group_id);
+        map.put("pro_type", pro_type);
+        map.put("statistic_ep_id", statistic_ep_id);
+        map.put("date_time", date_time);
+        map.put("condition", condition);
+        map.put("pro_name", pro_name);
+        map.put("start", start);
+        map.put("end", end);
+        map.put("export_name", export_name);
+        map.put("filename", filename);
+        map.put("record_start", record_start);
+        map.put("record_count", record_count);
+        map.put(EpConstant.EpKey.CORE_EP_ID, getAttribute(EpConstant.EpKey.CORE_EP_ID));
+        disposeSupplierPlatform(ep_type,ep_name,map);
+        return epOrderService.selectSupplierOrderConsumeDetail(map);
     }
 
     @RequestMapping(value = "supplier/refund", method = RequestMethod.GET)
     @ResponseBody
-    public Result selectSupplierOrderRefund(Integer ep_id,Integer pro_type,
+    public Result selectSupplierOrderRefund(Integer ep_type, String ep_name,Integer ep_id, Integer pro_type,
                                             @RequestParam("statistic_ep_id") Integer statistic_ep_id,
                                             @RequestParam("statistics_type_id") String statistics_type_id,
-                                            String start  ,String end,Integer group_id,String pro_name,
-                                            String export_name,String filename,
-                                            Integer record_start,Integer record_count){
-        Map<String,Object> map = new HashMap<>();
-        if(null==pro_type){
-            pro_type= ProductConstants.ProductType.SCENERY;
+                                            String start, String end, Integer group_id, String pro_name,
+                                            String export_name, String filename,
+                                            Integer record_start, Integer record_count) {
+        Map<String, Object> map = new HashMap<>();
+        if (null == pro_type) {
+            pro_type = ProductConstants.ProductType.SCENERY;
         }
-        map.put("ep_id",ep_id);
-        map.put("group",group_id);
-        map.put("pro_type",pro_type);
-        map.put("statistic_ep_id",statistic_ep_id);
-        map.put("key",statistics_type_id);
-        map.put("start",start);
-        map.put("end",end);
-        map.put("pro_name",pro_name);
-        map.put("export_name",export_name);
-        map.put("filename",filename);
-        map.put("record_start",record_start);
-        map.put("record_count",record_count);
-        map.put( EpConstant.EpKey.CORE_EP_ID,getAttribute( EpConstant.EpKey.CORE_EP_ID));
-        return  epOrderService.selectSupplierOrderRefund(map);
+        map.put("ep_id", ep_id);
+        map.put("group", group_id);
+        map.put("pro_type", pro_type);
+        map.put("statistic_ep_id", statistic_ep_id);
+        map.put("key", statistics_type_id);
+        map.put("start", start);
+        map.put("end", end);
+        map.put("pro_name", pro_name);
+        map.put("export_name", export_name);
+        map.put("filename", filename);
+        map.put("record_start", record_start);
+        map.put("record_count", record_count);
+        map.put(EpConstant.EpKey.CORE_EP_ID, getAttribute(EpConstant.EpKey.CORE_EP_ID));
+        disposeSupplierPlatform(ep_type,ep_name,map);
+        return epOrderService.selectSupplierOrderRefund(map);
     }
 
     @RequestMapping(value = "supplier/refund/detail", method = RequestMethod.GET)
     @ResponseBody
-    public Result selectSupplierOrderRefund(Integer ep_id,Integer pro_type,
+    public Result selectSupplierOrderRefund(Integer ep_type, String ep_name,Integer ep_id, Integer pro_type,
                                             @RequestParam("statistic_ep_id") Integer statistic_ep_id,
-                                            String start  ,String end,Integer group_id,
-                                            String date_time  ,String condition,String pro_name,
-                                            String filename,String export_name,
-                                            Integer record_start,Integer record_count){
-        Map<String,Object> map = new HashMap<>();
-        if(null==pro_type){
-            pro_type= ProductConstants.ProductType.SCENERY;
+                                            String start, String end, Integer group_id,
+                                            String date_time, String condition, String pro_name,
+                                            String filename, String export_name,
+                                            Integer record_start, Integer record_count) {
+        Map<String, Object> map = new HashMap<>();
+        if (null == pro_type) {
+            pro_type = ProductConstants.ProductType.SCENERY;
         }
-        map.put("ep_id",ep_id);
-        map.put("group",group_id);
-        map.put("pro_type",pro_type);
-        map.put("statistic_ep_id",statistic_ep_id);
-        map.put("date_time",date_time);
-        map.put("condition",condition);
-        map.put("pro_name",pro_name);
-        map.put("start",start);
-        map.put("end",end);
-        map.put("export_name",export_name);
-        map.put("filename",filename);
-        map.put("record_start",record_start);
-        map.put("record_count",record_count);
-        map.put( EpConstant.EpKey.CORE_EP_ID,getAttribute( EpConstant.EpKey.CORE_EP_ID));
-        return  epOrderService.selectSupplierOrderRefundDetail(map);
+        map.put("ep_id", ep_id);
+        map.put("group", group_id);
+        map.put("pro_type", pro_type);
+        map.put("statistic_ep_id", statistic_ep_id);
+        map.put("date_time", date_time);
+        map.put("condition", condition);
+        map.put("pro_name", pro_name);
+        map.put("start", start);
+        map.put("end", end);
+        map.put("export_name", export_name);
+        map.put("filename", filename);
+        map.put("record_start", record_start);
+        map.put("record_count", record_count);
+        map.put(EpConstant.EpKey.CORE_EP_ID, getAttribute(EpConstant.EpKey.CORE_EP_ID));
+        disposeSupplierPlatform(ep_type,ep_name,map);
+        return epOrderService.selectSupplierOrderRefundDetail(map);
+    }
+
+    private void disposeSupplierPlatform(Integer ep_type, String ep_name, Map map) {
+        if (ep_type != null && ep_type.intValue() == EpConstant.EpType.SUPPLIER.intValue()) {
+            if (ep_name != null) {
+                if (CommonUtil.find("\\(平台商\\)$", ep_name)) {
+                    Object temp=map.get("statistic_ep_id");
+                    map.put("statistic_ep_id",map.get(EpConstant.EpKey.CORE_EP_ID));
+                    map.put(EpConstant.EpKey.CORE_EP_ID,temp);
+                }
+            }
+        }
     }
 }
