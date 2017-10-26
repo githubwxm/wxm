3dd943903b43f33937b7a6c82eb6c28fc27a0e10
zoujing <731023100@qq.com>
2017-10-26 14:28:44 +0800

    feat(order): 新增团队相关查询接口

--- order-api/src/main/java/com/all580/order/api/service/GroupService.java
+++ order-api/src/main/java/com/all580/order/api/service/GroupService.java
@@ -1,6 +1,7 @@
 package com.all580.order.api.service;
 
 import com.framework.common.Result;
+import com.framework.common.vo.PageRecord;
 
 import java.util.Map;
 
@@ -26,4 +27,20 @@ public interface GroupService {
     Result<?> addGroupMember(Map params);
 
     Result<?> delGroupMember(Map params);
+
+    Result<?> queryGroupList(Integer core_ep_id,String number,String guide_name,String start,String end,String province,String city,Integer record_start,Integer record_count);
+
+    Result<?> queryGuideList(Integer core_ep_id,String name,String phone,String card,Integer record_start,Integer record_count);
+
+    Result<Map> queryGroupById(Integer id);
+
+    Result<Map> queryGuideById(Integer id);
+
+    Result<?> queryMemberList(Integer group_id,String name,String card,String phone,Integer record_start,Integer record_count);
+
+    Result<?> queryMemberNoPageList(Integer group_id);
+
+    Result<?> queryOrderGuideByOrderId(Integer orderId);
+
+    Result<?> queryOrderMember(Integer suborderid,String name,String card,String phone);
 }
